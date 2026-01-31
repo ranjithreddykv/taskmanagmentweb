ea83b74a5431b0fd9a8e7950c4044382c23534bd
Ranjith Reddy KV <ranjithreddykv27@gmail.com>
2026-01-31 13:52:36 +0530

    utils index.js updated

--- server/utils/index.js
+++ server/utils/index.js
@@ -18,8 +18,8 @@ export const createJWT = (res, userid) => {
   const isProduction = process.env.NODE_ENV === "production";
   res.cookie("token", token, {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 1 * 24 * 60 * 60 * 1000,
   });
 };
