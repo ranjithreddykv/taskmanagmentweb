d3efcd04498491cadc69638e8b57853364d90c9c
Ranjith Reddy KV <ranjithreddykv27@gmail.com>
2026-01-31 13:04:28 +0530

    cors error fixed

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin : ["https://taskmanweb.netlify.app/","http://localhost:3000","http://localhost:5173"] ,
+    origin : ["https://taskmanweb.netlify.app","http://localhost:3000","http://localhost:5173"] ,
     methods : ["GET","POST","PUT","DELETE"],
     credentials : true
 }))
